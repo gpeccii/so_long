ca772c1ccf1129ee07da2b00c11acbabbb3617de
Gianmarco Pecci <gpecci@e4r3p1.42roma.it>
2023-02-28 19:41:38 +0100

    da aggiungere schermata vittoria

--- win.c
+++ win.c
@@ -6,7 +6,7 @@
 /*   By: gpecci <gpecci@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 12:33:41 by gpecci            #+#    #+#             */
-/*   Updated: 2023/02/28 19:28:51 by gpecci           ###   ########.fr       */
+/*   Updated: 2023/02/28 19:41:20 by gpecci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,7 @@ void	win_s(t_game *game)
 	draw_wall(game);
 	ft_putstr("HAI VINTO!!!!");
 	ft_putstr("\n");
+	//mlx_string_put(game->mlx, game->win, (game->map_w) / 2, (game->map_h) / 2, 16777215, "HAI VINTO!!!!");
 }
 
 void	win_a(t_game *game)
